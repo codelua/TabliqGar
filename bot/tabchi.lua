7c40cdd9bd3205aa16dc36000f0680a280e7a513
empire2394 <kozeygoney0474@gmail.com>
2020-06-19 01:17:09 +0430

    Update lua.tabchi
    ok

--- bot/tabchi.lua
+++ bot/tabchi.lua
@@ -1,6 +1,6 @@
 tabchi = dofile('./bot/funcation.lua')
 --------------------------------
-tabchi_id = 302538792
+tabchi_id = 1230585497
 ---------------------------------
 json = dofile('./libs/JSON.lua')
 ---------------------------------
@@ -14,7 +14,7 @@ https = require "ssl.https"
 -----~~~~~~~~~~~~~~~~
 d = dofile('./libs/redis.lua')
 -------------------------------
-config_sudo = {280061509,310217440,363936960,366831302,56693692,260604473}
+config_sudo = {973895598,947126516}
 function dl_cb(arg, data)
 end
 function is_sudo(msg)
@@ -380,7 +380,7 @@ savecontact disable
 join enable 
 فعال کردن جويين خودکار
 join disable 
-غيرفعال کردن جوين خودکار
+غيرفعال کرددن جوين خودکار
 block (id)
 بلاک کردن کاربر 
 unblock (id)
